d1c5ecb51a463aeb430c0054e1f99c0ea554c975
EC2 Default User <ec2-user@ip-172-31-38-153.ap-northeast-1.compute.internal>
2020-03-18 07:35:59 +0000

    課題

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true
-    
-    has_secure_password
+  validates :name, presence: true, length: {maximum: 15}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  VALID_PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/
+  validates :password, presence: true, length: {in: 8..32}, format: { with: VALID_PASSWORD_REGEX }
+  
+  has_secure_password
 end
