9ca6f9d6bf2fff46ad13b9b486f454cbce1caf21
EC2 Default User <ec2-user@ip-172-31-38-153.ap-northeast-1.compute.internal>
2020-03-19 06:45:21 +0000

    課題

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # get 'users/new'
   root 'pages#index'
   get 'pages/help'
   
   resources :users
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy' 
 end
